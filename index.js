126832e4f7928a9b463272f2bdcdc525a2d434d2
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-06-11 19:30:26 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+
+function findMatching(arrayName, driverName){
+    return arrayName.filter((driverNames) => driverNames.toLowerCase() === driverName.toLowerCase());
+}
+function fuzzyMatch(names, stringName){
+    return names.filter(
+        (driverName) =>
+          driverName.toLowerCase().indexOf(stringName.toLowerCase()) === 0
+      );
+}
+function matchName(driverobj, argument){
+    return driverobj.filter((drivers) => drivers.name === argument);
+}
